fbe4dcf0d61076d3cd3992dea6efe167aed6a5f8
anhkhoi <anhkhoi1599@gmail.com>
2023-03-16 19:16:53 +0700

    index admin

---
+++ admin/index.php
@@ -0,0 +1,15 @@
+<?php include 'inc/header.php'; ?>
+<!-- End of Sidebar -->
+<div id="content-wrapper" class="d-flex flex-column">
+    <div id="content">
+        <div id="content-manage">
+            <a href="#">
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                Chào mừng bạn đến với luonvuituoi Store !!!
+            </a>
+        </div>
+    </div>
+</div>
